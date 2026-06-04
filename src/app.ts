bb18ed35fdf9a9325e1dbf950703651271bbf8d4
platane <me@platane.me>
2026-06-05 00:00:08 +0200

    👷

--- src/app.ts
+++ src/app.ts
@@ -7,45 +7,41 @@ import { getSpriteSheet } from "./worldRenderer/sprites";
 const canvas = document.getElementById("main-canvas") as HTMLCanvasElement;
 
 const renderer = createWorldRenderer(canvas, {
-	spriteSheet: await getSpriteSheet(),
+  spriteSheet: await getSpriteSheet(),
 });
 
 mat4.lookAt(renderer.viewMatrix, [1, 5, 10], [0, 0, 0], [0, 1, 0]);
 
 window.onresize = () =>
-	renderer.resize(
-		window.innerWidth,
-		window.innerHeight,
-		Math.min(window.devicePixelRatio ?? 1, 2),
-	);
+  renderer.resize(window.innerWidth, window.innerHeight, Math.min(window.devicePixelRatio ?? 1, 2));
 (window as any).onresize();
 
 let t = 0;
 
 const state = {
-	nodes: [
-		{ dead: false, hat: false, mailbox: [] },
-		{ dead: false, hat: false, mailbox: [] },
-		{ dead: false, hat: false, mailbox: [] },
-		{ dead: false, hat: false, mailbox: [] },
-		{ dead: false, hat: false, mailbox: [] },
-		{ dead: false, hat: false, mailbox: [] },
-		{ dead: false, hat: true, mailbox: [] },
-		{ dead: true, hat: true, mailbox: [] },
-	],
-	inbound: Array.from({ length: 100 }).map((_, i, { length }) => ({
-		sender: 0,
-		receiver: 1,
-		message: 0,
-		k: i / (length - 1),
-	})),
+  nodes: [
+    { dead: false, hat: false, mailbox: [] },
+    { dead: false, hat: false, mailbox: [] },
+    { dead: false, hat: false, mailbox: [] },
+    { dead: false, hat: false, mailbox: [] },
+    { dead: false, hat: false, mailbox: [] },
+    { dead: false, hat: false, mailbox: [] },
+    { dead: false, hat: true, mailbox: [] },
+    { dead: true, hat: true, mailbox: [] },
+  ],
+  inbound: Array.from({ length: 100 }).map((_, i, { length }) => ({
+    sender: 0,
+    receiver: 1,
+    message: 0,
+    k: i / (length - 1),
+  })),
 };
 
 const loop = () => {
-	t++;
+  t++;
 
-	renderer.draw(state, t);
+  renderer.draw(state, t);
 
-	requestAnimationFrame(loop);
+  requestAnimationFrame(loop);
 };
 loop();
--- src/worldRenderer/utils.ts
+++ src/worldRenderer/utils.ts
@@ -1,7 +1,7 @@
 // Standard Wang hash
 export const hashInt = (n: number) => {
-	n = ((n >> 16) ^ n) * 0x45d9f3b;
-	n = ((n >> 16) ^ n) * 0x45d9f3b;
-	n = (n >> 16) ^ n;
-	return n;
+  n = ((n >> 16) ^ n) * 0x45d9f3b;
+  n = ((n >> 16) ^ n) * 0x45d9f3b;
+  n = (n >> 16) ^ n;
+  return n;
 };
