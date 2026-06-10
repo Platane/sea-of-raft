1c5f506ffc6cfcf537d8f8952151b9aeb29b4643
platane <me@platane.me>
2026-06-10 08:55:09 +0200

    log state

--- src/app.ts
+++ src/app.ts
@@ -10,19 +10,16 @@ import { getSpriteSheet } from "./worldRenderer/sprites";
 // import "./scripts/generateTileSet";
 
 const canvas = document.getElementById("main-canvas") as HTMLCanvasElement;
+const domLogs = document.getElementById("logs") as HTMLElement;
 
 const renderer = createRenderer(canvas, {
-	spriteSheet: await getSpriteSheet(),
+  spriteSheet: await getSpriteSheet(),
 });
 
 mat4.lookAt(renderer.viewMatrix, [1, 5, 10], [0, 0, 0], [0, 1, 0]);
 
 window.onresize = () =>
-	renderer.resize(
-		window.innerWidth,
-		window.innerHeight,
-		Math.min(window.devicePixelRatio ?? 1, 2),
-	);
+  renderer.resize(window.innerWidth, window.innerHeight, Math.min(window.devicePixelRatio ?? 1, 2));
 (window as any).onresize();
 
 let state = createState(3);
@@ -31,15 +28,23 @@ const reduceNodes = createReducerNodes(heartBeatReduce);
 (window as any).__state = state;
 
 const loop = () => {
-	state.date++;
-	state = reduceAnimation(state);
+  state.date++;
+  state = reduceAnimation(state);
 
-	if (state.date % 60 === 0)
-		state = reduceNodes(state as any, { type: "tic-nodes" });
+  if (state.date % 60 === 0) state = reduceNodes(state as any, { type: "tic-nodes" });
 
-	updateEntities(state, renderer.entities);
-	renderer.draw();
+  domLogs.innerText = JSON.stringify(
+    {
+      date: state.date,
+      inFlightMessages: state.inFlightMessages.map((x) => x.message),
+    },
+    null,
+    2,
+  );
 
-	requestAnimationFrame(loop);
+  updateEntities(state, renderer.entities);
+  renderer.draw();
+
+  requestAnimationFrame(loop);
 };
 loop();
--- src/nodes/heartBeat.ts
+++ src/nodes/heartBeat.ts
@@ -24,12 +24,14 @@ export const heartBeatReduce: ReduceNode<
   // trigger ping messages
   const jitter = s.id ** 7 % 3;
   const longTimeNoSee = 10 + jitter;
-  const retryDelay = 5 + jitter;
+  const retryDelay = 6 + jitter;
 
   for (const id of s.peers) {
     if (id === s.id) continue;
 
-    if (s.lastSeen[id] ?? (0 < s.date - longTimeNoSee && s.sent[id]) ?? 0 < s.date - retryDelay) {
+    const shouldPing = s.lastSeen[id] ? s.lastSeen[id] + longTimeNoSee < s.date : jitter < s.date;
+
+    if (shouldPing && (s.sent[id] ?? 0) + retryDelay < s.date) {
       s.sent[id] = s.date;
       s.outBox.push({ receiver: id, message: "ping" });
     }
